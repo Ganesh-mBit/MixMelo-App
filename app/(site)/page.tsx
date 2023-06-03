aa86f6a2544c2aa1ea40b59fd4bd653f97b443f6
Ganesh M <ganeshm@192.168.1.2>
2023-06-03 18:06:22 +0530

    All bade UI structre is done.

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -12,6 +12,16 @@ export default function Home() {
           </div>
         </div>
       </Header>
+      <div className="mt-2 mb-6 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">
+            Newest songs
+          </h1>
+        </div>
+        <div>
+          List of songs!
+        </div>
+      </div>
     </div>
   )
 }

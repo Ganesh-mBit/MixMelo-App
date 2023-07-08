6cf4c4d15eae3b3dc9876ba15939d19e244d4ab1
Ganesh M <ganeshm@Ganeshs-MacBook-Air.local>
2023-07-08 11:51:31 +0530

    Added player hook in MediaItem component.

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { Song } from '@/types';
 import useLoadImage from '@/hooks/useLoadImage';
 import Image from 'next/image';
+import usePlayer from '@/hooks/usePlayer';
 
 interface MediaItemProps {
   data: Song
@@ -12,13 +13,14 @@ interface MediaItemProps {
 
 const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
   const imageUrl = useLoadImage(data);
+  const player = usePlayer();
 
   const handleClick = () => {
     if (onClick) {
       return onClick(data.id)
     }
 
-    // TODO: Default turn on player
+    return player.setId(data.id);
   };
 
   return (
